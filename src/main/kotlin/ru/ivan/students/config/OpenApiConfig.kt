dc22f8813e23ae43019459e76a4e0b1422964321
Van Simonovich <issimonovich@edu.hse>
2022-04-09 17:31:26 +0300

    Tags and one to many fix accounts

--- src/main/kotlin/ru/ivan/students/config/OpenApiConfig.kt
+++ src/main/kotlin/ru/ivan/students/config/OpenApiConfig.kt
@@ -12,20 +12,6 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme
 
 @OpenAPIDefinition(info = Info(title = "Ассистент для студентов", description = "Курсовой проект БПИ193", version = "v1"))
 @SecurityScheme(
-    name = "security_auth",
-    type = SecuritySchemeType.OAUTH2,
-    flows = OAuthFlows(
-        authorizationCode = OAuthFlow(
-            authorizationUrl = "http://localhost:8484/auth/realms/test_realm/protocol/openid-connect/auth",
-            tokenUrl = "http://localhost:8484/auth/realms/test_realm/protocol/openid-connect/token",
-            scopes = [OAuthScope(name = "read", description = "read scope"), OAuthScope(
-                name = "write",
-                description = "write scope"
-            )]
-        )
-    )
-)
-@SecurityScheme(
     name = "apiKey",
     scheme = "bearer",
     bearerFormat = "JWT",
