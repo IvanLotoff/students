7332f7567b0216c5ed7a86757fb352af1442ec5b
Van Simonovich <issimonovich@edu.hse>
2022-04-11 23:59:02 +0300

    stepik check

--- src/main/kotlin/ru/ivan/students/service/StepikService.kt
+++ src/main/kotlin/ru/ivan/students/service/StepikService.kt
@@ -1,83 +1,86 @@
 package ru.ivan.students.service
 
-import okhttp3.*
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 import java.io.BufferedReader
-import java.io.IOException
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 
 
 class StepikService {
-    val url = "https://stepik.org/users/36369913"
-    var user = "eFn4HbMGvQDwHQszbGDIIIT6I5pxSfWWyjqoR3Qw"
-    var secret = "OtuEL5Z0PPmMCWhmVDA9eDIFebWhrIhFeDcRF1BNXvPS7WXH3vLXiYf13apqIIUDWN1Wifr0OpV6nf0fhKzAtBNkyJXMGkHlV17w69Bdffh3rRjoDq6Neq61iYOcOLkh"
 
-    @Throws(IOException::class)
     fun getAccessToken() {
-        // Create a new HTTP client
-        val client = OkHttpClient().newBuilder().build()
-
-        // Create the request body
-        val mediaType = MediaType.parse("text/plain")
-        val body = RequestBody.create(
-            mediaType,
-            "response_type=token&client_id=ClientId&username=user&password=userpassword&scope=process&grant_type=password"
+        val url = URL("https://stepik.org/oauth2/token/")
+        val http = url.openConnection() as HttpURLConnection
+        http.requestMethod = "POST"
+        http.doOutput = true
+        http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+        http.setRequestProperty(
+            "Authorization",
+            "Basic ZUZuNEhiTUd2UUR3SFFzemJHRElJSVQ2STVweFNmV1d5anFvUjNRdzpPdHVFTDVaMFBQbU1DV2htVkRBOWVESUZlYldockloRmVEY1JGMUJOWHZQUzdXWEgzdkxYaVlmMTNhcHFJSVVEV04xV2lmcjBPcFY2bmYwZmhLekF0Qk5reUpYTUdrSGxWMTd3NjlCZGZmaDNyUmpvRHE2TmVxNjFpWU9jT0xraA=="
         )
 
-        // Build the request object, with method, headers
-        val request: Request =Request.Builder()
-            .url("https://stepik.org/oauth2/token/")
-            .method("POST", body)
-            .addHeader("Authorization", createAuthHeaderString(user, secret)!!)
-            .build()
-        // Perform the request, this potentially throws an IOException
-        val response = client.newCall(request).execute()
-        println(response)
-    }
+        val data = "grant_type=client_credentials"
 
-    // Just a helper metod to create the basic auth header
-    private fun createAuthHeaderString(username: String, password: String): String? {
-        val auth = "$username:$password"
-        return "Basic $auth"
-    }
-
-    fun sendGet(): String {
-
-//        var doc: Document? = null
-//        try {
-//            doc = Jsoup.connect(url).get()
-//        } catch (e: IOException) {
-//            e.printStackTrace()
-//        }
-//        val title = doc!!.title()
-//        println(doc.text().split("—").first())
+        http.outputStream.write(data.toByteArray(Charsets.UTF_8))
 
 
-        val httpClient: HttpURLConnection = URL(url).openConnection() as HttpURLConnection
-        httpClient.requestMethod = "GET"
-        httpClient.setRequestProperty("User-Agent", "Mozilla/5.0")
-        var res: String
-        BufferedReader(InputStreamReader(httpClient.getInputStream())).use { `in` ->
-            val response = StringBuilder()
-            var line: String?
-            while (`in`.readLine().also { line = it } != null) response.append(line).append("\n")
-            res = response.toString()
+        println(http.responseCode.toString() + " " + http.responseMessage)
+
+        val sb = StringBuilder()
+        val br = BufferedReader(
+            InputStreamReader(http.getInputStream(), "utf-8")
+        )
+        var line: String? = null
+        while (br.readLine().also { line = it } != null) {
+            sb.append(
+                """
+            $line
+            
+            """.trimIndent()
+            )
         }
+        br.close()
+        System.out.println("" + sb.toString())
 
-        //a#member 66.ember-view.link-secondndary. link_no-static-line
-        var html: Document = Jsoup.parse(res)
+        http.disconnect()
 
 
-        return html.text()
     }
 
 //    fun sendGet(): String {
-//        val html: Document = Jsoup.connect(url).get()
-//        var res1= html.select("#ember56").first();
-//        return ""
+//
+////        var doc: Document? = null
+////        try {
+////            doc = Jsoup.connect(url).get()
+////        } catch (e: IOException) {
+////            e.printStackTrace()
+////        }
+////        val title = doc!!.title()
+////        println(doc.text().split("—").first())
+//
+//
+//        val httpClient: HttpURLConnection = URL(url).openConnection() as HttpURLConnection
+//        httpClient.requestMethod = "GET"
+//        httpClient.setRequestProperty("User-Agent", "Mozilla/5.0")
+//        var res: String
+//        BufferedReader(InputStreamReader(httpClient.getInputStream())).use { `in` ->
+//            val response = StringBuilder()
+//            var line: String?
+//            while (`in`.readLine().also { line = it } != null) response.append(line).append("\n")
+//            res = response.toString()
+//        }
+//
+//        //a#member 66.ember-view.link-secondndary. link_no-static-line
+//        var html: Document = Jsoup.parse(res)
+//
+//
+//        return html.text()
 //    }
+//
+////    fun sendGet(): String {
+////        val html: Document = Jsoup.connect(url).get()
+////        var res1= html.select("#ember56").first();
+////        return ""
+////    }
 }
 
